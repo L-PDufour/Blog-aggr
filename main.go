d2305d8e05ffb60514c5e65758eab84f9319f7c5
L-PDufour <leonpierre.dufour@gmail.com>
2024-05-24 14:25:39 -0400

    Create user

--- main.go
+++ main.go
@@ -1,27 +1,93 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/L-PDufour/Blog-aggr/internal/database"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(user database.User) User {
+	return User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Name:      user.Name,
+	}
+}
+
+func (cfg *apiConfig) handlerPostUsers(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	type response struct {
+		User
+	}
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithERROR(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      params.Name,
+	})
+
+	if err != nil {
+		respondWithERROR(w, http.StatusInternalServerError, "Couldn't create user")
+	}
+	respondWithJSON(w, http.StatusCreated, databaseUserToUser(user))
+
+}
 
 func main() {
 	err := godotenv.Load("./.env")
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
-
+	dbURL := os.Getenv("DB")
+	if dbURL == "" {
+		log.Fatalf("DB environment variable not set")
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatalf("PORT environment variable not set")
 	}
 
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+	cfg := &apiConfig{
+		DB: dbQueries,
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
 	mux.HandleFunc("GET /v1/err", handlerError)
+	mux.HandleFunc("POST /v1/users", cfg.handlerPostUsers)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
